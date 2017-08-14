5c511af262ec660251e8da83d69b6bf77641ee4e
Lee Walters <lee@touchyfeelymedia.com>
2017-08-14 16:53:38 +0100

    Create pmf-stripe-subscribe.js

---
+++ pmf-stripe-subscribe.js
@@ -0,0 +1,14 @@
+var stripe = require('stripe@4.14.0');
+
+module.exports = function(context, cb) {
+  stripe(context.secrets.stripe_private_api_key).subscriptions.create({
+    customer: context.body.customer-id,
+    items: [
+      {
+        plan: "pmf",
+      },
+    ],
+  }, (err, subscription) => {
+    cb(null, { status: err ? 400 : 200, subscription: subscription });
+  });
+};
